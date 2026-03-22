ca7e6a10f2a462fcd2a733271b32c4a365f7d7d7
Muhammad Saad <saadmughal532@gmail.com>
2026-03-22 17:09:00 +0500

    Updated API URL

--- lib/data/code_review_repo_impl.dart
+++ lib/data/code_review_repo_impl.dart
@@ -3,12 +3,13 @@ import '../domain/repositories/code_review_repo.dart';
 
 class CodeReviewRepoImpl implements CodeReviewRepo {
   final Dio dio = Dio();
+  final String baseUrl = "";
 
   @override
   Future<String> getCodeReview(String code) async {
     try {
       final response = await dio.post(
-        "https://devlens-nzkh.onrender.com/api/review",
+        baseUrl,
         data: {"code": code},
         options: Options(headers: {"Content-Type": "application/json"}),
       );
